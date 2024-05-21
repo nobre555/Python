38e669c51cbbc00db138d647b30bab0279983917
nobre555 <dmmnobre@gmail.com>
2024-05-21 19:12:02 +0100

    Exercicio 5 terminado

--- Aulas/Ficha1.py
+++ Aulas/Ficha1.py
@@ -1,2 +1,31 @@
-#Pergunta 1:
+#Ficha de Atividades 2
+#Exercicio 1:
+nome_do_utilizador=input("Insira o seu nome:")
+print("Olá,",nome_do_utilizador)
 
+#Exercicio 2:
+float=0,5
+String=Diogo
+int=3
+boolean=True
+
+#Exercicio 3
+PI=3,14
+numero=6
+resultado = PI*numero
+print(resultado)
+
+#Exercicio 4 
+num1=int(input("Insira um número "))
+num2=int(input("Insira outro numero "))
+
+subtracao=num1-num2
+soma=num1+num2
+multiplicacao= num1*num2
+divisao=num1/num2
+print(num1,"+" , num2, "=" ,soma)
+print(num1,"-" , num2, "=" ,subtracao)
+print(num1,"*" , num2, "=" ,multiplicacao)
+print(num1,"/" , num2, "=" ,divisao)
+
+#Exercicio5
