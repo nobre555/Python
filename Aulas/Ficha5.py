4f1d16c9787424a0a8e72bf83809a85729b8a8f4
nobre555 <dmmnobre@gmail.com>
2024-06-07 19:31:14 +0100

    ficha4

---
+++ Aulas/Ficha5.py
@@ -0,0 +1,45 @@
+#Exercicio1
+class Ponto:
+    x=0
+    y=0
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def alterar_pontos(x,y):
+        self.x=x
+        self.y=y
+    def obter_valores():
+        print(x,y)
+    def calcula_distancia():
+        return print((0-self.x)+ (0-self.y))
+
+#Exercicio2
+class Livro:
+    titulo
+    autor 
+    num_pag
+
+    def __init__(self,titulo,autor,num_pag):
+        self.titulo=titulo
+        self.autor=autor
+        self.num_pag=num_pag
+
+    def info(livro):
+        print("Titulo:",self.titulo,"Autor:",self.autor,"Número de Páginas:",num_pag)
+
+#Exercicio3
+class Telemovel:
+    marca
+    modelo 
+    capacidade 
+    armazenamento_utilizado
+
+    def __init__(self,marca,modelo,capacidade):
+        self.marca=marca
+        self.modelo=modelo
+        self.capacidade=capacidade
+    
+    def calcula_capacidade():
+        return capacidade-armazenamento_utilizado
+
+#Exercicio4
